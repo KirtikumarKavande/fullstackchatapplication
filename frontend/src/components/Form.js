288801e625b039e46692ae630e336dcf99831134
KirtikumarKavande <kirtikumar0005@gmail.com>
2023-09-08 19:37:34 +0530

    some strater file added

---
+++ frontend/src/components/Form.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <div className="rel">
+      <img
+        src="https://images.unsplash.com/photo-1528460033278-a6ba57020470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+        alt="bg"
+      />
+
+      <form className="absolute">
+        <input />
+      </form>
+    </div>
+  );
+};
+
+export default Form;
