76f0c80071ff4d85da7b1b849fde2227619a71df
KirtikumarKavande <kirtikumar0005@gmail.com>
2025-10-28 15:07:44 +0530

    feat: create simple docker file for to check all work fine or not

--- backend/util/database.js
+++ backend/util/database.js
@@ -1,6 +1,24 @@
+// const Sequelize = require("sequelize");
+// const sequelize = new Sequelize("chatapp", "root", "Kolhapur@64", {
+//   dialect: "mysql",
+//   host: "localhost",
+// });
+// module.exports = sequelize;
+
+
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("chatapp", "root", "Kolhapur@64", {
-  dialect: "mysql",
-  host: "localhost",
-});
+
+const sequelize = new Sequelize(
+  "pass",
+  {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging: false,
+  }
+);
+
 module.exports = sequelize;
